81e41005590086f02addcf8298bdd80a34eba345
Jeisilin07 <162548647+Jeisilin07@users.noreply.github.com>
2024-04-05 20:40:38 +0530

    CreateLead.java

--- CreateLead.java
+++ CreateLead.java
@@ -14,20 +14,20 @@ public class CreateLead {
 		driver.get("https://login.salesforce.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("jeisjohn07@testleaf.com");
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Jeis1607"); 
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("learners@testleaf.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Leaf@1234"); 
 		driver.findElement(By.xpath("//input[@id='Login']")).click();
         driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
-        driver.findElement(By.xpath("//button[@class='slds button']")).click();
+        driver.findElement(By.xpath("//button[@class='slds-button']")).click();
         driver.findElement(By.xpath("//p[text()='Sales']")).click();
         WebElement lead = driver.findElement(By.xpath("//span[text()='Leads'][1]"));
         driver.executeScript("arguments[0].click()",lead);
-        WebElement drop = driver.findElement(By.xpath("//button[@role='combobox'])[3]"));
+        driver.findElement(By.xpath("//div[@title='New']")).click();
+        WebElement drop = driver.findElement(By.xpath("(//button[@role='combobox'])[3]"));
         driver.executeScript("arguments[0].click()",drop);
-        driver.findElement(By.xpath("//span[text()='Mr.']")).click();
         driver.findElement(By.xpath("//label[text()='Last Name']/following::input[1]")).sendKeys("Kumar");
         driver.findElement(By.xpath("//label[text()='Company']/following::input[1]")).sendKeys("Testleaf");
-        driver.findElement(By.xpath("//button[text()='Save']")).click();
+        driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
         
 	}
 
