ded3d0c128bed21389f8fc16f994b2f809413ff5
Jeisilin07 <162548647+Jeisilin07@users.noreply.github.com>
2024-04-09 16:51:36 +0530

    EditLead.java

--- EditLead.java
+++ EditLead.java
@@ -18,15 +18,15 @@ public class EditLead {
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("jeis1607"); 
 		driver.findElement(By.xpath("//input[@id='Login']")).click();
 		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
-	        driver.findElement(By.xpath("//button[@class='slds-button']")).click();
-	        driver.findElement(By.xpath("//p[text()='Sales']")).click();
-	        WebElement lead = driver.findElement(By.xpath("//span[text()='Leads'][1]"));
-	        driver.executeScript("arguments[0].click()",lead);
-		driver.findElement(By.xpath("//a[@title='Edit']")).click();
+	    driver.findElement(By.xpath("//button[@class='slds-button']")).click();
+	    driver.findElement(By.xpath("//p[text()='Sales']")).click();
+	    WebElement lead = driver.findElement(By.xpath("//span[text()='Leads'][1]"));
+	    driver.executeScript("arguments[0].click()",lead);
+	    driver.findElement(By.xpath("(//button[contains(@class,'slds-button slds-button_icon-border')]//lightning-primitive-icon)[2]")).click();
+	    driver.findElement(By.xpath("//a[@title='Edit']")).click();
 		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Ganesh");
-		WebElement scroll = driver.findElement(By.xpath("//button[@aria-label='Lead Status-Current Selection:New']"));
-		driver.executeScript("arguments[0].click();",scroll);
-		driver.findElement(By.xpath("//span[@title='Contacted']")).click();
+		WebElement element = driver.findElement(By.xpath("(//button[@class='slds-combobox__input slds-input_faux fix-slds-input_faux slds-combobox__input-value'])[5]"));
+		driver.executeScript("arguments[0].click();",element);
 		driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
        
 	}
